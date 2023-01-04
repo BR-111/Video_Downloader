4dc754301a069dfea5fdfe97e427f9f587db5e1d
Braulio Parra <79633566+BR-111@users.noreply.github.com>
2023-01-03 22:10:59 -0800

    Add files via upload

---
+++ VD.py
@@ -0,0 +1,19 @@
+
+from pytube import YouTube
+import os 
+from pathlib import Path
+
+link = str(input(" Enter Link: "))
+url = YouTube(link)
+
+print(" Downlaoding ...")
+
+# Getting Highest Resolution
+video = url.streams.get_highest_resolution()
+
+path_to_download_folder = str(os.path.join(Path.home(), "Downloads"))
+
+video.download(path_to_download_folder)
+print(" Downloaded!")
+
+
