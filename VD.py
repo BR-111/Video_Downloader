8e9b90167896e6c5c8a95dc568523829aacb8274
Braulio Parra <79633566+BR-111@users.noreply.github.com>
2023-01-06 12:43:28 -0800

    Update VD.py

--- VD.py
+++ VD.py
@@ -3,17 +3,17 @@ from pytube import YouTube
 import os 
 from pathlib import Path
 
-link = str(input(" Enter Link: "))
-url = YouTube(link)
-
-print(" Downlaoding ...")
-
-# Getting Highest Resolution
-video = url.streams.get_highest_resolution()
-
-path_to_download_folder = str(os.path.join(Path.home(), "Downloads"))
-
-video.download(path_to_download_folder)
-print(" Downloaded!")
+def main_func():
+  link = str(input(" Enter Link: "))
+  url = YouTube(link)
+  print(" Downlaoding ...")
+  # Getting Highest Resolution
+  video = url.streams.get_highest_resolution()
+  path_to_download_folder = str(os.path.join(Path.home(), "Downloads"))
+  video.download(path_to_download_folder)
+  print(" Downloaded!")
 
 
+if __name__ == "__main__":
+  main_func()
+  
